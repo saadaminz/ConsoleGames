4d8988323a0202a059b5ac41b9ef88d05ab59dc8
Saad Amin <saadaminc@gmail.com>
2025-03-27 23:27:30 +0500

    Add project files.

---
+++ Games/src/Snake.cpp
@@ -0,0 +1,172 @@
+
+#if 0
+
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+/*
+Button mapping:
+
+	W = UP
+	S = DOWN
+	A = LEFT
+	D = RIGHT
+	Escape = Exit game
+*/
+
+constexpr int size = 25;
+
+struct Segments
+{
+	int posY, posX;
+};
+
+struct Snake
+{
+	int snakeY, snakeX;
+	int fruitY, fruitX;
+	int fruitsCount;
+	int score, speed;
+	char grid[size][size];
+	std::vector<Segments> body;
+
+	enum Move { UP = 0, DOWN, RIGHT, LEFT };
+	Move move = LEFT;
+	Move lastMove = move;
+
+	Snake()
+		: snakeY(17), snakeX(17), score(0), fruitsCount(0), speed(80)
+	{
+		memset(grid, '_', sizeof(grid));
+		grid[snakeY][snakeX] = '0';
+		body.reserve(25);
+		body.push_back({ snakeY, snakeX });
+		std::srand(std::time(nullptr));
+		GRand();
+	}
+
+	~Snake() {}
+
+	void GotoXY()
+	{
+		COORD c;
+		c.X = 0;
+		c.Y = 0;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	}
+
+	void GRand()
+	{
+		fruitsCount++;
+
+		while (true)
+		{
+			fruitY = (rand() % (size - 2)) + 1;
+			fruitX = (rand() % (size - 2)) + 1;
+
+			if (grid[fruitY][fruitX] == '_')
+				break;
+		}
+
+		if ((fruitsCount % 5) == 0)
+			grid[fruitY][fruitX] = '$';
+		else
+			grid[fruitY][fruitX] = '%';
+	}
+
+	void RunLoop()
+	{
+		while (true)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				grid[i][0] = '#';
+				grid[0][i] = '#';
+				grid[i][size - 1] = '#';
+				grid[size - 1][i] = '#';
+			}
+
+			for (int i = 0; i < body.size(); i++)
+				grid[body[i].posY][body[i].posX] = '_';
+
+			if (body.size() > 1)
+				for (int i = body.size() - 1; i > 0; i--)
+					body[i] = body[i - 1];
+
+			grid[snakeY][snakeX] = '_';
+
+			if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) return;
+			else if (GetAsyncKeyState(0x41/*A*/) & 0x8000 && lastMove != RIGHT)	move = LEFT;
+			else if (GetAsyncKeyState(0x44/*D*/) & 0x8000 && lastMove != LEFT)	move = RIGHT;
+			else if (GetAsyncKeyState(0x57/*W*/) & 0x8000 && lastMove != DOWN)	move = UP;
+			else if (GetAsyncKeyState(0x53/*S*/) & 0x8000 && lastMove != UP)	move = DOWN;
+			lastMove = move;
+
+			switch (move)
+			{
+			case LEFT:	snakeX--; break;
+			case RIGHT:	snakeX++; break;
+			case UP:	snakeY--; break;
+			case DOWN:	snakeY++; break;
+			}
+
+			body[0].posY = snakeY;
+			body[0].posX = snakeX;
+
+			for (int i = 1; i < body.size(); i++)
+			{
+				if ((body[i].posY == snakeY) && (body[i].posX == snakeX))
+				{
+					system("cls");
+					std::cout << "Game Over!\nYour Score: " << score << '\n';
+					return;
+				}
+			}
+
+			if (grid[snakeY][snakeX] == '#')
+			{
+				system("cls");
+				std::cout << "Game Over!\nYour Score: " << score << '\n';
+				return;
+			}
+
+			if (snakeY == fruitY && snakeX == fruitX)
+			{
+				if ((fruitsCount % 5) == 0) score += 15;
+				else score += 5;
+
+				GRand();
+				if (speed > 30) speed -= 3;
+				Segments newSegment = body.back();
+				body.push_back(newSegment);
+			}
+
+			for (int i = 0; i < body.size(); i++)
+				grid[body[i].posY][body[i].posX] = (i == 0) ? '0' : 'O';
+
+			char buffer[size * size + 100];
+			int bufferIndex = 0;
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+					buffer[bufferIndex++] = grid[i][j];
+				buffer[bufferIndex++] = '\n';
+			}
+			buffer[bufferIndex++] = '\0';
+			printf("%sScore: %d\n", buffer, score);
+
+			Sleep(speed);
+			GotoXY();
+		}
+	}
+};
+
+int main()
+{
+	Snake game;
+	game.RunLoop();
+}
+
+#endif
---
+++ Games/src/Tetris.cpp
@@ -0,0 +1,278 @@
+
+#if 0
+
+#include <iostream>
+#include <Windows.h>
+#include <array>
+#include <thread>
+
+/*
+Button mapping:
+
+	A = LEFT
+	D = RIGHT
+	S = Increase speed
+	Spacebar = Rotate piece
+	Escape = Exit game
+*/
+
+constexpr int size = 20;
+char grid[size][size];
+int speed = 100, speedUp = 0;
+int score = 0;
+bool gameOver = false;
+
+struct Tetromino
+{
+	char block[4][4];
+	int posY, posX;
+};
+
+Tetromino testPiece;
+Tetromino currentPiece;
+Tetromino* nextPiece = nullptr;
+
+Tetromino I = { {{' ','#',' ',' '}, {' ','#',' ',' '}, {' ','#',' ',' '}, {' ','#',' ',' '}} };
+Tetromino L = { {{' ','#',' ',' '}, {' ','#',' ',' '}, {' ','#','#',' '}, {' ',' ',' ',' '}} };
+Tetromino J = { {{' ',' ','#',' '}, {' ',' ','#',' '}, {' ','#','#',' '}, {' ',' ',' ',' '}} };
+Tetromino T = { {{' ',' ',' ',' '}, {'#','#','#',' '}, {' ','#',' ',' '}, {' ',' ',' ',' '}} };
+Tetromino O = { {{' ',' ',' ',' '}, {' ','#','#',' '}, {' ','#','#',' '}, {' ',' ',' ',' '}} };
+Tetromino S = { {{' ',' ',' ',' '}, {' ',' ','#','#'}, {'#','#',' ',' '}, {' ',' ',' ',' '}} };
+Tetromino Z = { {{' ',' ',' ',' '}, {'#','#',' ',' '}, {' ',' ','#','#'}, {' ',' ',' ',' '}} };
+
+std::array<Tetromino*, 7> pieces = { &I,&L,&J,&T,&O,&S,&Z };
+
+enum Movement { NILL=0, RIGHT, LEFT, SPACEBAR };
+Movement move = NILL;
+
+Tetromino* GetRandomPiece()
+{
+	int index = std::rand() % pieces.size();
+	pieces[index]->posY = 0;
+	pieces[index]->posX = size / 2;
+	return pieces[index];
+}
+
+void GotoXY()
+{
+	COORD c;
+	c.Y = 0;
+	c.X = 0;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+bool ValidatePosition(const Tetromino& T)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (T.block[i][j] == ' ')
+				continue;
+
+			int pY = T.posY + i;
+			int pX = T.posX + j;
+
+			if (pX < 0 || pX >= size || pY >= size)
+				return false;
+
+			if (grid[pY][pX] != '_')
+				return false;
+		}
+	}
+	return true;
+}
+
+
+
+void RotatePiece(const Tetromino& moveFrom, Tetromino& moveTo)
+{
+	memset(moveTo.block, ' ', sizeof(moveTo.block));
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			moveTo.block[3 - j][i] = moveFrom.block[i][j];
+		}
+	}
+}
+
+void DrawInfo()
+{
+	std::cout << "\nNext Piece:\n";
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (nextPiece->block[i][j] == ' ')
+				std::cout << '_';
+			else
+				std::cout << nextPiece->block[i][j];
+		}
+		std::cout << '\n';
+	}
+	std::cout << "\nScore: " << score << '\n';
+}
+
+void DrawGrid()
+{
+	char buffer[size * size + 100];
+	int index = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			buffer[index++] = grid[i][j];
+		}
+		buffer[index++] = '\n';
+	}
+
+	buffer[index++] = '\0';
+	printf("%s", buffer);
+}
+
+void DrawPieceOnGrid(const Tetromino& T)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (T.block[i][j] != ' ')
+			{
+				grid[T.posY + i][T.posX + j] = T.block[i][j];
+			}
+		}
+	}
+}
+
+void RemovePieceTrackFromGrid(Tetromino& T)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (T.block[i][j] != ' ')
+			{
+				grid[T.posY + i][T.posX + j] = '_';
+			}
+		}
+	}
+}
+
+void ClearLine()
+{
+	for (int i = size - 1; i >= 0; i--)
+	{
+		bool lineFull = true;
+
+		for (int j = 0; j < size; j++)
+		{
+			if (grid[i][j] == '_')
+			{
+				lineFull = false;
+				break;
+			}
+		}
+
+		if (lineFull)
+		{
+			for (int row = i; row > 0; row--)
+			{
+				for (int col = 0; col < size; col++)
+				{
+					grid[row][col] = grid[row - 1][col];
+				}
+			}
+
+			for (int col = 0; col < size; col++)
+				grid[0][col] = '_';
+
+			score += 45;
+			i++;
+		}
+	}
+}
+
+void RunLoop()
+{
+	std::srand(std::time(nullptr));
+	memset(grid, '_', sizeof(grid));
+	nextPiece = GetRandomPiece();
+	currentPiece = *nextPiece;
+
+	while (!gameOver)
+	{
+		nextPiece = GetRandomPiece();
+
+		while (!gameOver)
+		{
+			testPiece = currentPiece;
+			speedUp = 0;
+
+			if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)		return;
+			else if (GetAsyncKeyState(0x53/*S*/) & 0x8000)	speedUp = 50;
+			else if (GetAsyncKeyState(0x44/*D*/) & 0x8000)	move = RIGHT;
+			else if (GetAsyncKeyState(0x41/*A*/) & 0x8000)	move = LEFT;
+			else if (GetAsyncKeyState(VK_SPACE) & 0x8000)	move = SPACEBAR;
+			else move = NILL;
+
+			if (move != NILL)
+			{
+				if (move == RIGHT)			testPiece.posX++;
+				else if (move == LEFT)		testPiece.posX--;
+				else if (move == SPACEBAR)	RotatePiece(currentPiece, testPiece);
+
+				if (!ValidatePosition(testPiece))
+					testPiece = currentPiece;
+				else
+				{
+					DrawPieceOnGrid(currentPiece);
+					DrawGrid();
+					DrawInfo();
+					RemovePieceTrackFromGrid(currentPiece);
+					GotoXY();
+					currentPiece = testPiece;
+				}
+			}
+
+			testPiece.posY++;
+
+			if (ValidatePosition(testPiece))
+			{
+				DrawPieceOnGrid(currentPiece);
+				DrawGrid();
+				DrawInfo();
+				RemovePieceTrackFromGrid(currentPiece);
+				currentPiece = testPiece;
+			}
+			else
+			{
+				DrawPieceOnGrid(currentPiece);
+				ClearLine();
+				score += 15;
+				if (speed > 70) speed--;
+				currentPiece = *nextPiece;
+
+				if (!ValidatePosition(currentPiece))
+				{
+					system("cls");
+					std::cout << "Game Over!\nYour Score: " << score << '\n';
+					gameOver = true;
+				}
+				break;
+			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(speed - speedUp));
+			GotoXY();
+		}
+	}
+}
+
+int main()
+{
+	RunLoop();
+}
+
+#endif
